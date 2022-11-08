08ab67a304545c725d31543b57a6108d09b7dccd
Bang241002 <bang241002@gmail.com>
2022-11-08 17:32:48 +0700

    ch3

---
+++ ch3/02-meadowlark.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const expressHandlebars = require('express-handlebars');
+const app = express();
+// configure Handlebars view engine
+app.engine(
+    'handlebars',
+    expressHandlebars.engine({
+        defaultLayout: 'main',
+        // extname: '.hbs',
+    })
+);
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => res.render('home'));
+app.get('/about', (req, res) => res.render('about'));
+// custom 404 page
+app.use((req, res) => {
+    res.status(404);
+    res.render('404');
+});
+// custom 500 page
+app.use((err, req, res, next) => {
+    console.error(err.message);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(3000);
